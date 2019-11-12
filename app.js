0e6cd58694c693621de71a2a62b64aede3450ccf
jjapa <konis123@naver.com>
2019-11-13 00:19:21 +0900

    error correct

--- app.js
+++ app.js
@@ -154,3 +154,28 @@ io.sockets.on('connection', function (socket) {
       }
   });
 });
+
+function onNewNamespace(channel, sender) {
+  io.of('/' + channel).on('connection', function (socket) {
+      var username;
+      if (io.isConnected) {
+          io.isConnected = false;
+          socket.emit('connect', true);
+      }
+
+      socket.on('message', function (data) {
+          if (data.sender == sender) {
+              if(!username) username = data.data.sender;
+              
+              socket.broadcast.emit('message', data.data);
+          }
+      });
+      
+      socket.on('disconnect', function() {
+          if(username) {
+              socket.broadcast.emit('user-left', username);
+              username = null;
+          }
+      });
+  });
+}
